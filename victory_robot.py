987eccc72c271b9cad3f7acdedf5a6fe3108d064
Axel Brisse <axel.brisse@gmail.com>
2024-11-27 23:53:07 +0100

    106

---
+++ victory_robot.py
@@ -0,0 +1,24 @@
+# ruff: noqa: E731, E741
+import sys
+
+read = sys.stdin.readline
+input = lambda: read().strip()
+ir = lambda: int(read())
+rir = lambda: range(int(read()))
+mir = lambda: map(int, read().split())
+lmir = lambda: list(map(int, read().split()))
+
+
+def main():
+    for _ in rir():
+        n = ir()
+        if n == 1971:
+            print("Joy Bangla")
+        elif n % 2 == 0:
+            print("Bangla")
+        else:
+            print("Joy")
+
+
+if __name__ == "__main__":
+    main()
