a91a08dd0a1b7ee6542134c9ff4e378b926af7ab
Axel Brisse <axel.brisse@gmail.com>
2024-11-28 21:32:02 +0100

    122

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## interactive problems to fix
 
 - [ ] https://toph.co/p/an-obvious-interactive-problem
+- [ ] https://toph.co/p/a-new-tough-game
---
+++ aquaman_s_greatest_army.py
@@ -0,0 +1,18 @@
+# ruff: noqa: E731, E741
+import sys
+
+read = sys.stdin.readline
+input = lambda: read().strip()
+ir = lambda: int(read())
+rir = lambda: range(int(read()))
+mir = lambda: map(int, read().split())
+lmir = lambda: list(map(int, read().split()))
+
+
+def main():
+    read()
+    print(sum(~n & 1 for n in mir()))
+
+
+if __name__ == "__main__":
+    main()
