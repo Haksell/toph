7d03435e718786eb7da5bbcfaf6315538123071a
Axel Brisse <axel.brisse@gmail.com>
2024-11-28 22:39:11 +0100

    .

---
+++ count_the_mines.py
@@ -0,0 +1,17 @@
+# ruff: noqa: E731, E741
+import sys
+
+read = sys.stdin.readline
+input = lambda: read().strip()
+ir = lambda: int(read())
+rir = lambda: range(int(read()))
+mir = lambda: map(int, read().split())
+lmir = lambda: list(map(int, read().split()))
+
+
+def main():
+    print(sum(line.count("m") for line in sys.stdin))
+
+
+if __name__ == "__main__":
+    main()
