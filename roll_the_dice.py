20d6e4c73a6b164faf315e81660f544d8e04674d
Axel Brisse <axel.brisse@gmail.com>
2024-11-28 21:15:25 +0100

    .

---
+++ roll_the_dice.py
@@ -0,0 +1,19 @@
+# ruff: noqa: E731, E741
+import sys
+
+read = sys.stdin.readline
+input = lambda: read().strip()
+ir = lambda: int(read())
+rir = lambda: range(int(read()))
+mir = lambda: map(int, read().split())
+lmir = lambda: list(map(int, read().split()))
+
+
+def main():
+    for _ in rir():
+        n = ir()
+        print(6 * n, n)
+
+
+if __name__ == "__main__":
+    main()
